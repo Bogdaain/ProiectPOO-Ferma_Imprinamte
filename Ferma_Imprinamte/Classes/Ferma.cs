0a1bdb1291126610b3e7c83526c8ce7c5c29a6b9
Bogdein <bogdan.ghiurliu-burdean@student.upt.ro>
2025-01-04 11:04:07 +0200

    Adaugare Invantar la Ferma

--- Ferma_Imprinamte/Classes/Ferma.cs
+++ Ferma_Imprinamte/Classes/Ferma.cs
@@ -2,5 +2,22 @@ namespace Ferma_Imprinamte.Classes;
 
 public class Ferma
 {
-    
+    private List<Printer> printere;
+
+    public Ferma()
+    {
+        printere = new List<Printer>();
+    }
+
+    public string InventarImprimante()
+    {
+        if(printere.Count == 0)
+            return "Nu exista imprimante disponibile";
+        string inventar = "Imprimante:\n";
+        foreach (var priner in printere)
+        {
+            inventar += $"ID:{printer.CNP}, Status:{printer.Status}\n";
+        }
+        return inventar;
+    }
 }
