0a726613cb24d53b9c9da68877ceca84c8eab962
Bogdein <bogdan.ghiurliu-burdean@student.upt.ro>
2025-01-04 12:20:34 +0200

    Inceput Program

--- Ferma_Imprinamte/Program.cs
+++ Ferma_Imprinamte/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Ferma_Imprinamte;
-
+﻿using Ferma_Imprinamte.Classes;
+namespace Ferma_Imprinamte;
 class Program
 {
+    static List<Printer> printere = new List<Printer>();
+    static List<string> comenzi = new List<string>();
+    static Dictionary<string, double> stocFilament = new Dictionary<string, double>();
+    static double costPerGramRasina = 0.5;
+    static double costPerGramPlastic = 0.3;
     static void Main(string[] args)
     {
-        Console.WriteLine("Ceau Bogdan");
+        
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Alegeto o optiune: ");
+            Console.WriteLine("1. Utilizator\\n2. Administrator\\n3. Iesire");
+            string optiune = Console.ReadLine();
+
+            switch (optiune)
+            {
+                case "1":
+                    break;
+                    
+            }
+        }
     }
 }
