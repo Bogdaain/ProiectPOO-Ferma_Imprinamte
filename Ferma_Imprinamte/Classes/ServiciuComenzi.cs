082cd02f3c6c12809fa6ff95a0ed82b523859319
Bogdein <bogdan.ghiurliu-burdean@student.upt.ro>
2025-01-04 10:37:39 +0200

    adaugat serviciu comanzi

---
+++ Ferma_Imprinamte/Classes/ServiciuComenzi.cs
@@ -0,0 +1,22 @@
+namespace Ferma_Imprinamte.Classes;
+
+public class ServiciuComenzi
+{
+    private List<Comanda> comenzi;
+
+    public ServiciuComenzi()
+    {
+        comenzi = new List<Comanda>();
+    }
+
+    public string CreareComanda(string nume, double greutate, string culoare, string adresa)
+    {
+        Comanda comandaNoua = new Comanda
+        {
+            Numa = nume,
+            Greutate = greutate,
+            Culoare = culoare,
+            Adresa = adresa
+        };
+    }
+}
