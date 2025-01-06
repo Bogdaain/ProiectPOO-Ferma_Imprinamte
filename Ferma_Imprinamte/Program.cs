ab80713637b94396f47f9c2ce47fbf6dc80e6852
gheor <gheorghescusimina9@gmail.com>
2025-01-06 11:44:13 +0200

    meniu utilizator

--- Ferma_Imprinamte/Program.cs
+++ Ferma_Imprinamte/Program.cs
@@ -2,6 +2,7 @@
 namespace Ferma_Imprinamte;
 class Program
 {
+    private static Ferma fermaImprimante = new Ferma();
     public static void Main(string[] args)
     {
         while (true)
@@ -23,6 +24,74 @@ class Program
                     break;
                 case "3":
                     Console.WriteLine("La revedere!");
+                    return;
+                default:
+                    Console.WriteLine("Optiune invalida. Incearca din nou.");
+                    break;
+            }
+        }
+    }
+
+    private static void MeniuUtilizator()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Meniu Utilizator:");
+            Console.WriteLine("1. Vizualizare costuri");
+            Console.WriteLine("2. Calculeaza cost obiect");
+            Console.WriteLine("3. Plaseaza o comanda");
+            Console.WriteLine("4. Inapoi la meniul principal");
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine(fermaImprimante.VizualizareCosturi());
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Console.WriteLine(fermaImprimante.VizualizareImprimante());
+                    Console.WriteLine("Introdu CNP-ul imprimantei pentru calculul costului: ");
+                    if (int.TryParse(Console.ReadLine(), out int cnpImprimanta))
+                    {
+                        Console.WriteLine("Introdu greutatea obiectului: ");
+                        if (double.TryParse(Console.ReadLine(), out double greutate))
+                        {
+                            Console.WriteLine(fermaImprimante.CalculeazaCostObiect(greutate, cnpImprimanta));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Greutate invalidă.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("CNP invalid.");
+                    }
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    Console.WriteLine("Introdu numele obiectului: ");
+                    string numeObiect = Console.ReadLine();
+                    Console.WriteLine("Introdu greutatea obiectului: ");
+                    if (double.TryParse(Console.ReadLine(), out double greutateComanda))
+                    {
+                        Console.WriteLine("Introdu culoarea obiectului: ");
+                        string culoareObiect = Console.ReadLine();
+                        Console.WriteLine("Introdu adresa de livrare: ");
+                        string adresaComanda = Console.ReadLine();
+
+                        Console.WriteLine(fermaImprimante.PlaseazaComanda(numeObiect, greutateComanda,
+                            culoareObiect, adresaComanda));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Greutate invalida.");
+                    }
+
+                    Console.ReadKey();
+                    break;
+                case "4":
                     return;
                 default:
                     Console.WriteLine("Optiune invalida. Incearca din nou.");
