ac7e62cfecbe02c38ded502bdc406097e6d0be93
Danny <danielgabor2018@yahoo.com>
2025-01-06 11:51:10 +0200

    Am incept sa scriu Meniul de Administrator

--- Ferma_Imprinamte/Program.cs
+++ Ferma_Imprinamte/Program.cs
@@ -99,4 +99,55 @@ class Program
             }
         }
     }
+    private static void MeniuAdministrator()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Meniu Administrator:");
+            Console.WriteLine("1. Vizualizare status imprimante");
+            Console.WriteLine("2. Vizualizare detalii imprimanta");
+            Console.WriteLine("3. Adaugare rasina");
+            Console.WriteLine("4. Vizualizare stoc filament");
+            Console.WriteLine("5. Adauga filament in stoc");
+            Console.WriteLine("6. Inapoi la meniul principal");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine(fermaImprimante.VizualizareImprimante());
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Console.Write("Introdu CNP-ul imprimantei: ");
+                    if (int.TryParse(Console.ReadLine(), out int cnp))
+                    {
+                        Printer imprimanta = null;
+
+                        foreach (var imprimantaCurenta in fermaImprimante.printere)
+                        {
+                            if (imprimantaCurenta.CNP == cnp)
+                            {
+                                imprimanta = imprimantaCurenta;
+                                break;
+                            }
+                        }
+                        if (imprimanta != null)
+                        {
+                            Console.WriteLine(fermaImprimante.DetaliiImprimanta(cnp));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Imprimanta nu a fost gasita.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("CNP invalid.");
+                    }
+                    break;
+            }
+        }
+    }
 }
